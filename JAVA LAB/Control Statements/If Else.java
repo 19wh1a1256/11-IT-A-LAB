a54a4df6388974ef7c2c02132a6e0aac66a7e2dd
19wh1a1256 <70095745+19wh1a1256@users.noreply.github.com>
2021-06-24 18:36:32 +0530

    Create If Else.java

---
+++ JAVA LAB/Control Statements/If Else.java
@@ -0,0 +1,16 @@
+public class IfElse
+{  
+public static void main(String[] args)
+{  
+    int number=1201;  
+    if(number % 2 == 0)
+    {  
+        System.out.println("Even");  
+    }
+    else
+    {  
+        System.out.println("Odd");  
+    }  
+}  
+}  
+© 2021 GitHub, Inc.
