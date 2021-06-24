326e3b4da9eeb163e10bd5d95ad6fe776e414189
19wh1a1256 <70095745+19wh1a1256@users.noreply.github.com>
2021-06-24 18:24:32 +0530

    Create Parameterized Constructor.java

---
+++ JAVA LAB/constructors/Parameterized Constructor.java
@@ -0,0 +1,28 @@
+//parameterized constructor
+class Base
+{
+Base(int x)
+{
+int a=x;
+System.out.println("Values of a is "+a);
+}
+Base()
+{
+System.out.println("Base class constructor");
+}
+}
+class Child extends Base
+{
+Child()
+{
+super(100);
+System.out.println("child class constructor");
+}
+}
+class ParameterizedConstructor
+{
+public static void main(String[] args)
+{
+Child obj=new Child();
+}
+}
