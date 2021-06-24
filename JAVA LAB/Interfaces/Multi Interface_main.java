d2107bdc35a5bb405de491c0658cf89dec606b99
19wh1a1256 <70095745+19wh1a1256@users.noreply.github.com>
2021-06-24 18:10:22 +0530

    Create Multi Interface_main.java

---
+++ JAVA LAB/Interfaces/Multi Interface_main.java
@@ -0,0 +1,21 @@
+interface Bank{
+void accountNumber();
+}
+interface Loan{
+void getRoI();
+}
+class Multi implements Bank,Loan{
+public void accountNumber(){
+System.out.println("details of account");
+}
+public void getRoI(){
+System.out.println("rate of interest");
+}
+}
+class MultiInt_main{
+public static void main(String args[]){
+Multi m = new Multi();
+m.accountNumber();
+m.getRoI();
+}
+}
