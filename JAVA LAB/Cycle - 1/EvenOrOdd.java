10f7900b3d98b5c2bf360cef0bf534479b871ae5
19wh1a1256 <70095745+19wh1a1256@users.noreply.github.com>
2021-06-25 18:59:58 +0530

    Create EvenOrOdd.java

---
+++ JAVA LAB/Cycle - 1/EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOrOdd {
+   public static void main(String args[]){
+      //Reading number from user
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a number ::" );
+      int num = sc.nextInt();
+     
+      //Finding even or odd
+      if (num % 2 == 0){
+         System.out.println("Entered number is an even number");
+      } else {
+         System.out.println("Entered number is an odd number");
+      }
+   }
+}
