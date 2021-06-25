f28e4dc52e57604b71d015e98dac4bec8eb21b65
19wh1a1256 <70095745+19wh1a1256@users.noreply.github.com>
2021-06-25 08:01:37 +0530

    Update Break.java

--- JAVA LAB/Control Statements/Break.java
+++ JAVA LAB/Control Statements/Break.java
@@ -1,31 +1,14 @@
-class Base  //HIERARCHICAL INHERITANCE
-{
-	int a = 10;
-}
-class Child extends Base  
-{
-	int b = 20;
-	void sum()
-	{
-		System.out.println("Sum of a and b is : " +(a + b));
-	}
-}
-
-class ChildChild extends Child
-{
-	int c = 5;
-	void mul()
-	{
-		System.out.println("Multiplication of a and b and c is : " +(a * b * c));
-	}
-}
-class Hierarchical
-{
-public static void main(String[] args)
-{
-	Child obj1 = new Child();
-	obj1.sum();
-	ChildChild obj2 = new ChildChild();
-	obj2.mul();
-}
-}
+public class Break
+{  
+    public static void main(String[] args)
+    {    
+        for(int i=1;i<=10;i++)
+	{  
+            if(i==5)
+	    {    
+                break;  
+            }  
+            System.out.println(i);  
+        }  
+    }  
+}  
